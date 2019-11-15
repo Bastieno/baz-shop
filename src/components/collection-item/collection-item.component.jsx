cf3d2985f6e07030860bfaa81df2dd29edc60664
Francis Nduamaka <francis.nduamaka@gmail.com>
2019-11-15 11:06:12 +0100

    update cart reducer to take care of duplicates efficiently

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addItemToCart } from '../../redux/cart/cart.actions';
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, imageUrl, price }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, imageUrl, price } = item;
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -9,8 +13,18 @@ const CollectionItem = ({ id, name, imageUrl, price }) => {
         <span className="name"> {name} </span>
         <span className="price"> ${price} </span>
       </div>
+      <CustomButton 
+        inverted
+        onClick={() => addItem(item)}
+      >
+        Add to cart
+      </CustomButton>
     </div>
   )
 }
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItemToCart(item)) 
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
