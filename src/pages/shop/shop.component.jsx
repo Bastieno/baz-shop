708ee5abfc05e704914a7a7fa90e6ef1ebda6029
Francis Nduamaka <francis.nduamaka@gmail.com>
2019-11-16 18:44:10 +0100

    create the collection page and the collection-overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { createStructuredSelector } from 'reselect';
-import { connect } from 'react-redux';
-import { selectCollections } from '../../redux/shop/shop.selectors';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { Route } from 'react-router-dom';
+import CollectionPage from '../collection/collection.component';
+import CollectionsOverview from '../../components/collection-overview/collections-overview.component';
 import './shop.styles.scss';
 
-const ShopPage = ({ collections }) => (
-  <div className="shop-page">
-    {
-      collections.map(({ id, ...otherCollectionProps}) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))
-    }
-  </div>
-)
+const ShopPage = ({ match }) => {
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  )
+}
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-})
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
